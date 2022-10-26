93b4894e11dce7187386246c7b549c72353ec9c5
DevJihwan <jh552016@naver.com>
2022-10-26 12:06:17 +0900

    feat : polygon mumbai network 연결 및 hardhat task 추가 ("account")

--- ds_contracts/hardhat.config.js
+++ ds_contracts/hardhat.config.js
@@ -1,6 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
